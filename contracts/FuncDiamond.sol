72ed0cff603a67cbad754e433c086f293de705ef
yuki <y.takahashi2020@gmail.com>
2023-09-02 09:55:29 +0900

    first commit

---
+++ contracts/FuncDiamond.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity 0.8.18;
+
+import "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
+
+contract FuncDiamond is SolidStateDiamond {}
---
+++ contracts/MessageFacet.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity 0.8.18;
+
+contract MessageFacet {
+    
+}
